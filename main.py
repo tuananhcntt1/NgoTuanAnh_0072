e354bc798bbd5a5a8f5629545966ec1258582923
NgoTuanAnh <tuananh300604@.gmail.com>
2026-03-14 22:05:10 +0700

    cong2so

---
+++ main.py
@@ -0,0 +1,11 @@
+# Nhập số thứ nhất từ bàn phím và chuyển sang kiểu số thực (float)
+so_thu_nhat = float(input("Nhập số thứ nhất: "))
+
+# Nhập số thứ hai
+so_thu_hai = float(input("Nhập số thứ hai: "))
+
+# Tính tổng
+tong = so_thu_nhat + so_thu_hai
+
+# Hiển thị kết quả
+print(f"Tổng của {so_thu_nhat} và {so_thu_hai} là: {tong}")
